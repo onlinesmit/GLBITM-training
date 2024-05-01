d0187938d900d746d439e4577f83c0e0a7c22a42
onlinesmit <onlinesmitg@gmail.com>
2024-05-01 20:54:18 +0530

    Gl bajaj traing by Anirudhh sir Ex Amazon SE

---
+++ basics/1stDay.js
@@ -0,0 +1,58 @@
+// let name = "Smit";
+// let age = 23;
+// let workAs = "Software engineer";
+
+// console.log("Name is " + name + " & age is " + age);
+// console.log(`Name is ${name} & age is ${age} & Working as ${workAs}`);
+// console.log(3+3+3);
+// console.log(typeof workAs);
+
+// let isLoggedin = "Smit";
+// let booleanIsLogged = Boolean(isLoggedin);
+// console.log(booleanIsLogged);
+
+// let isnum = 12;
+// console.log(typeof isnum);  //number
+// let stringnum = String(isnum);  
+// console.log(stringnum);  //12
+// console.log(typeof stringnum);  //String
+
+// let value = 3;
+// let negValue = -value;
+
+// console.log(value);
+// console.log(negValue);
+
+// let str1 = "Hello";
+// let str2 = "Everyone";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);  //12
+// console.log("1  " + 2);  //1   2
+// console.log("1" + 2 + 2);  //122
+// console.log("1" + 2 + "2");  //122
+// console.log(1 + 2 + "2");  //32
+// console.log(1 + 2 + (5%3));  // 5
+
+// console.log("true"); //true
+// console.log(+true);  //1
+// console.log(+"");  //0
+
+// let getc = 1;
+// ++getc;
+// console.log(getc);  //2
+
+console.log(null > 0);  //false
+console.log(null == 0);
+console.log(null === 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined === 0);
+
+console.log( 1 == 1);  //true
+console.log( 1 === 1);
+
